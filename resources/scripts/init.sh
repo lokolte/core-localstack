a6ad178620c7a20ea1616ff03b22e2ca9a9b9cd6
Jesus Aguilar <jesus.aguilar@weightwatchers.com>
2019-06-03 14:37:15 -0400

    Adding permission to s3 test data file and added logging to init.sh.

--- resources/scripts/init.sh
+++ resources/scripts/init.sh
@@ -5,18 +5,23 @@ read -ra ADDR <<< "$LSTACK_SERVICES"
 for service_th in "${ADDR[@]}"; do
     if [ $service_th == 's3' ]
     then
-        awslocal cloudformation create-stack --template-body file://${DATADIR}/templates/s3template.yml --stack-name teststack
+        awslocal cloudformation create-stack --template-body file://${DATADIR}/templates/s3template.yml --stack-name teststack >> /tmp/localstack_infra.log
 
         #To create a bucket
-        echo -e "Creating bucket $LSTACK_BUCKET for server $LSTACK_HOST in port $LSTACK_PORT"
-        aws --endpoint-url=http://${LSTACK_HOST}:${LSTACK_PORT} s3 mb s3://${LSTACK_BUCKET}
+        echo -e "Creating bucket $LSTACK_BUCKET for server $LSTACK_HOST in port $LSTACK_PORT" >> /tmp/localstack_infra.log
+        aws --endpoint-url=http://${LSTACK_HOST}:${LSTACK_PORT} s3 mb s3://${LSTACK_BUCKET} >> /tmp/localstack_infra.log
+
+        #Grant full access permission for public access
+        echo -e "Adding full access permission to s3_test_data.json file." >> /tmp/localstack_infra.log
+        aws --endpoint-url=http://${LSTACK_HOST}:${LSTACK_PORT} s3api put-bucket-acl --bucket ${LSTACK_BUCKET} --acl public-read >> /tmp/localstack_infra.log
+        aws --endpoint-url=http://${LSTACK_HOST}:${LSTACK_PORT} s3api put-object-acl --bucket ${LSTACK_BUCKET} --key s3_test_data.json --grant-full-control uri=http://acs.amazonaws.com/groups/global/AllUsers >> /tmp/localstack_infra.log
 
         #To copy files into s3 localstack
-        echo -e "Copying s3_test_data content to $DATADIR/data"
-        aws --endpoint-url=http://${LSTACK_HOST}:${LSTACK_PORT} s3 cp ${DATADIR}/resources/local_data/s3_test_data.json s3://${LSTACK_BUCKET}
+        echo -e "Copying s3_test_data content to $DATADIR/data" >> /tmp/localstack_infra.log
+        aws --endpoint-url=http://${LSTACK_HOST}:${LSTACK_PORT} s3 cp ${DATADIR}/local_data/s3_test_data.json s3://${LSTACK_BUCKET}/ >> /tmp/localstack_infra.log
 
         #To list the files in s3
-        echo -e "Lising content of $LSTACK_BUCKET"
-        aws --endpoint-url=http://${LSTACK_HOST}:${LSTACK_PORT} s3 ls s3://${LSTACK_BUCKET}
+        echo -e "Lising content of $LSTACK_BUCKET" >> /tmp/localstack_infra.log
+        aws --endpoint-url=http://${LSTACK_HOST}:${LSTACK_PORT} s3 ls s3://${LSTACK_BUCKET} >> /tmp/localstack_infra.log
     fi
 done
